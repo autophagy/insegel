9fa47ac2c0dd40d9a8d0ddf427d478c3a7065ef0
Autophagy <mika@crate.io>
2017-11-17 03:04:02 +0100

    Removed 2.7 support (Datarum is 3+)

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         'Environment :: Console',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
