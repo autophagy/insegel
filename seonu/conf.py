b8e93c219db4e8cc604c45efe1e37707caa8e5cc
Autophagy <mika@crate.io>
2019-04-29 21:34:46 +0200

    Add CSS for python autodocs

--- seonu/conf.py
+++ seonu/conf.py
@@ -17,3 +17,5 @@ html_last_updated_fmt = '%d %b %Y'
 releases_github_path = 'autophagy/insegel'
 # Our pre-0.x releases are unstable / mix bugs+features
 releases_unstable_prehistory = True
+
+extensions = ['sphinx.ext.autodoc']
